45954cb705e6db5b1c4cfeffd3fdf2456f5fc39c
Han Li <hali@bu.edu>
2024-09-26 23:55:58 -0400

    initial commit

--- image_compression_impl.py
+++ image_compression_impl.py
@@ -4,11 +4,24 @@ from PIL import Image
 
 # Function to load and preprocess the image
 def load_image(image_path):
-    raise NotImplementedError('You need to implement this function')
+    image = Image.open(image_path)
+    return np.array(image)
 
 # Function to perform KMeans clustering for image quantization
 def image_compression(image_np, n_colors):
-    raise NotImplementedError('You need to implement this function')
+    pixels = image_np.reshape(-1, 3)
+
+    # Apply KMeans clustering to reduce the number of colors
+    kmeans = KMeans(n_clusters=n_colors, random_state=42)
+    kmeans.fit(pixels)
+
+    # Replace each pixel with its corresponding cluster center (compressed color)
+    compressed_pixels = kmeans.cluster_centers_[kmeans.labels_].astype(np.uint8)
+
+    # Reshape the compressed pixels back to the original image shape
+    compressed_image = compressed_pixels.reshape(image_np.shape)
+
+    return compressed_image
 
 # Function to concatenate and save the original and quantized images side by side
 def save_result(original_image_np, quantized_image_np, output_path):
@@ -31,12 +44,12 @@ def save_result(original_image_np, quantized_image_np, output_path):
 
 def __main__():
     # Load and process the image
-    image_path = 'favorite_image.png'  
-    output_path = 'compressed_image.png'  
+    image_path = './joe.jpg'
+    output_path = './compressed_image.png'
     image_np = load_image(image_path)
 
     # Perform image quantization using KMeans
-    n_colors = 8  # Number of colors to reduce the image to, you may change this to experiment
+    n_colors = 2  # Number of colors to reduce the image to, you may change this to experiment
     quantized_image_np = image_compression(image_np, n_colors)
 
     # Save the original and quantized images side by side
